fb683fbaede01c0cdfaaf16632bc849e2047d37e
suk-6 <me@suk.kr>
2024-12-08 00:04:55 +0900

    fix(start.sh): update docker command to use 'docker compose'

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 git pull origin main
 git submodule foreach git pull origin main --force
 git submodule update --init --recursive --remote
-docker-compose up -d --build
+docker compose up -d --build
